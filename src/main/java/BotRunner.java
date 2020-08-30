5ccc0cbaf1907abcc08309ae4d664333992556a8
Josh <ddiamonds@joshbl.dev>
2020-08-29 21:25:12 -0400

    Jar

--- src/main/java/BotRunner.java
+++ src/main/java/BotRunner.java
@@ -5,7 +5,6 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.gmail.Gmail;
@@ -33,7 +32,7 @@ import java.util.Set;
 
 public class BotRunner {
     private static final String APPLICATION_NAME = "Schwartz Bot";
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final JacksonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
     // Sheet IDs
